3f47b29cbea6896bc24a01b93f0b6bb40cc285ae
jyc0410 <yoonchul.jeong00@gmail.com>
2014-04-20 22:28:25 +0900

    Naming rules, align, fixed some errors

--- src/com/He/W/onebone/Circuit/Cu/Level.java
+++ src/com/He/W/onebone/Circuit/Cu/Level.java
@@ -2,6 +2,7 @@ package com.He.W.onebone.circuit.cu;
 
 
 public class Level{
-public Level(String MapName, int start, int end, Component[] mapcomponent, Component[] items){
-}
+	public Level(String MapName, int start, int end, Component[] mapcomponent, Component[] items){
+		
+	}
 }
---
+++ src/com/He/W/onebone/Circuit/Cu/LevelReader.java
@@ -0,0 +1,15 @@
+package com.He.W.onebone.circuit.cu;
+
+import java.util.HashMap;
+
+public class LevelReader{
+	public HashMap<String,Level> Levels = new HashMap<String,Level>();
+	
+	public static void readAllLevels(){
+		
+	}
+	
+	public static void writeSuccessData(int time,String LevelName){
+		
+	}
+}
