63efa3ce4072dda23fa3369f3bab81a467baf2b2
HelloWorld017 <deu07115@gmail.com>
2014-04-24 17:04:17 +0900

    Updated LevelHelper
    Updated LevelHelper
    
    Introducing Electricity Blocker as COMPONENT
    2 days ago, Electricity Blocker was just abstract class but now, it is
    component

--- src/com/He/W/onebone/circuit/cu/LevelHelper.java
+++ src/com/He/W/onebone/circuit/cu/LevelHelper.java
@@ -54,7 +54,25 @@ public class LevelHelper{
 									1 = Author
 									2 = Start Voltage or RPM
 									3 = End Voltage or RPM
-									4 = MapType (Wire or Cogs) */
+									4 = MapType (Wire or Cogs or Mixed) */
+				ArrayList<Integer> al_itemdata = new ArrayList<Integer>();
+				/*					al_itemdata index
+				 * 					0 = wire
+				 * 					1 = Electricity Blocker
+				 * 					2 = Resister
+				 * 					3 = Transistor
+				 * 					4 = WireToCog
+				 * 					5 = CogToWire
+				 * 					6 = Cog*/
+				ArrayList<Integer> al_componentdata = new ArrayList<Integer>();
+				/*					al_componentdata index
+				 * 					0 = wire(cog)
+				 * 					1 = Electricity Blocker
+				 * 					2 = Resister
+				 * 					3 = Transistor
+				 * 					4 = WireToCog
+				 * 					5 = CogToWire
+				 *					6 = Cog*/
 				ArrayList<String> Fullmap = new ArrayList<String>();
 				int msl = -1; //Mapdata Start Line
 				int mel = -1; //Mapdata End Line
@@ -86,7 +104,117 @@ public class LevelHelper{
 						Toast.makeText(ctxt, "Broken Level!", Toast.LENGTH_LONG).show();
 						return null;
 					}else{
-						//TODO: WRITE SCRIPT
+						for(int a = 0; a < Fullmap.size() - 1; a++){
+							//map data
+							if(msl < a && a < mel){
+								al_mapdata.add(a - msl, Fullmap.get(a));
+							}
+							
+							//item data
+							if(isl < a && a < iel){
+								String[] set = Fullmap.get(a).split(",");
+								
+								
+								//error check
+								try{
+									int test = Integer.valueOf(set[1]);
+								}catch(NumberFormatException e){
+									Toast.makeText(ctxt, "Broken Level : item amount is not number", Toast.LENGTH_LONG).show();
+									return null;
+								}
+								
+								//Cannot use switch because set[0] is String
+								
+								//item data add
+								if(set[0].equals("Wire")){
+									
+									al_itemdata.add(0,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("ElectricityBlocker")){
+									
+									al_itemdata.add(1,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("Resister")){
+									
+									al_itemdata.add(2,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("Transistor")){
+									
+									al_itemdata.add(3,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("Wiretocog")){
+									
+									al_itemdata.add(4,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("Cogtowire")){
+									
+									al_itemdata.add(5,Integer.valueOf(set[1]));
+									
+								}else if(set[0].equals("Cog")){
+									
+									al_itemdata.add(6,Integer.valueOf(set[1]));
+									
+								}else{
+								
+									
+									Toast.makeText(ctxt, "Broken Level : unknown item : " + set[0], Toast.LENGTH_LONG).show();
+									
+								}
+							}
+							
+							//component data
+							if(csl < a && a < cel){
+								String[] set2 = Fullmap.get(a).split(",");
+								
+								
+								//error check
+								try{
+									int test = Integer.valueOf(set2[1]);
+								}catch(NumberFormatException e){
+									Toast.makeText(ctxt, "Broken Level : item amount is not number", Toast.LENGTH_LONG).show();
+									return null;
+								}
+								
+								//Cannot use switch because set[0] is String
+								
+								//item data add
+								if(set2[0].equals("Wire")){
+									
+									al_componentdata.add(0,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("ElectricityBlocker")){
+									
+									al_componentdata.add(1,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("Resister")){
+									
+									al_componentdata.add(2,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("Transistor")){
+									
+									al_componentdata.add(3,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("Wiretocog")){
+									
+									al_componentdata.add(4,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("Cogtowire")){
+									
+									al_componentdata.add(5,Integer.valueOf(set2[1]));
+									
+								}else if(set2[0].equals("Cog")){
+									
+									al_componentdata.add(6,Integer.valueOf(set2[1]));
+									
+								}else{
+									
+									Toast.makeText(ctxt, "Broken Level : unknown component : " + set2[0], Toast.LENGTH_LONG).show();
+									
+								}
+							}
+							
+						}
+						//TODO: Make null to new level
 						return null;
 					}
 				
--- src/com/He/W/onebone/circuit/cu/component/EnumComponentType.java
+++ src/com/He/W/onebone/circuit/cu/component/EnumComponentType.java
@@ -1,5 +1,6 @@
 package com.He.W.onebone.circuit.cu.component;
 
 public enum EnumComponentType {
-	COMPONENT_RESISTER, COMPONENT_TRANSISTOR, COMPONENT_WIRE, COMPONENT_WIRETOCOG, COMPONENT_COGTOWIRE, COMPONENT_COG
+	COMPONENT_RESISTER, COMPONENT_TRANSISTOR, COMPONENT_WIRE, COMPONENT_WIRETOCOG, COMPONENT_COGTOWIRE, COMPONENT_COG, COMPONENT_ELECTRICITYBLOCKER
+	//Electricity Blocker is component that makes players not to wire at some position (make puzzles harder)
 }
