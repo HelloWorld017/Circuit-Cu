ae331c835d2b1a8badbdbcc9a5d70444ce97a197
HelloWorld017 <deu07115@gmail.com>
2014-06-11 22:27:30 +0900

    added Setting Parent

--- src/com/He/W/onebone/circuit/cu/android/SettingAdapter.java
+++ src/com/He/W/onebone/circuit/cu/android/SettingAdapter.java
@@ -3,6 +3,7 @@ package com.He.W.onebone.circuit.cu.android;
 import java.util.ArrayList;
 
 import com.He.W.onebone.circuit.cu.R;
+import com.He.W.onebone.circuit.cu.settings.EnumSettingParents;
 import com.He.W.onebone.circuit.cu.settings.EnumSettings;
 import com.He.W.onebone.circuit.cu.settings.Setting;
 import com.He.W.onebone.circuit.cu.settings.SettingSpefHelper;
@@ -10,6 +11,7 @@ import com.He.W.onebone.circuit.cu.settings.SettingSpefHelper;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -40,10 +42,11 @@ public class SettingAdapter extends BaseAdapter {
 		 * 5 = does setting needs restart
 		 * 6 = parent of setting
 		 * 7 = name of setting
+		 * 8 = is Parent
 		 */
-
+		ArrayList<EnumSettingParents> esp = new ArrayList<EnumSettingParents>();
 		for(int a = 0; a < es.length; a++){
-			Object[] obj = new Object[8];
+			Object[] obj = new Object[9];
 			EnumSettings esa = es[a];
 			obj[0] = esa;
 			obj[1] = ssh.getSettingId(esa);
@@ -53,6 +56,14 @@ public class SettingAdapter extends BaseAdapter {
 			obj[5] = ssh.isNeededRestart(esa);
 			obj[6] = ssh.getParent(esa);
 			obj[7] = ssh.getName(esa);
+			obj[8] = false;
+			if(!(esp.contains((EnumSettingParents)obj[6]))){
+				Object[] parent  = new Object[9];
+				parent[0] = obj[6];
+				parent[8] = true;
+				database.add(parent);
+				esp.add((EnumSettingParents) obj[6]);
+			}
 			database.add(obj);
 		}
 		Log.d("PrL1bug", "data : " + database.size());
@@ -83,10 +94,25 @@ public class SettingAdapter extends BaseAdapter {
 		if(arg1 == null){
 			arg1 = li.inflate(R.layout.custom_setting_list_view, arg2, false);
 		}
-
+		
+		
 		TextView settingName = (TextView) arg1.findViewById(R.id.lblSettingName);
 		TextView settingDesc = (TextView) arg1.findViewById(R.id.lblSettingDesc);
 		Button modify = (Button) arg1.findViewById(R.id.btnModify);
+		if(((Boolean)database.get(arg0)[8]).booleanValue()){
+			settingDesc.setVisibility(View.INVISIBLE);
+			modify.setVisibility(View.INVISIBLE);
+			String s = "";
+			switch((EnumSettingParents)database.get(arg0)[0]){
+			case LooknFeel:s = "Look & Feel";break;
+			default:s = ((EnumSettingParents)database.get(arg0)[0]).name();break;
+			}
+			settingName.setText(s);
+			Typeface tf = (Typeface) Setting.getPrefix(0);
+			settingName.setTypeface(tf);
+			settingName.setTextSize(50);
+			return arg1;
+		}	
 		settingName.setText(String.valueOf(database.get(arg0)[7]));
 		settingDesc.setText(String.valueOf(database.get(arg0)[2]));
 		modify.setOnTouchListener(new View.OnTouchListener() {
--- src/com/He/W/onebone/circuit/cu/settings/Setting.java
+++ src/com/He/W/onebone/circuit/cu/settings/Setting.java
@@ -4,19 +4,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
-import java.util.Locale;
 
 import com.He.W.onebone.circuit.cu.StackTraceToString;
 
 import android.content.Context;
-import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -83,7 +79,7 @@ public class Setting {
 		
 		//Writing part
 			for(int a = 0; a < flags.size();a++){
-				T2W = eslist[a] + "," + flags.get(eslist[a]);
+				T2W = eslist[a].name() + "," + flags.get(eslist[a]);
 				bw.write(T2W);
 				bw.newLine();
 			}
