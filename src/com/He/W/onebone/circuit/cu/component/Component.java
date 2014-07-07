458aefe0a9c300c3436ebe344ba881cd1b2408c9
onebone <yoonchul.jeong00@gmail.com>
2014-07-07 23:10:49 +0900

    Component revert
    - Component rewriting will proceed in new branch "Component-Rewrite"

--- src/com/He/W/onebone/circuit/cu/component/Component.java
+++ src/com/He/W/onebone/circuit/cu/component/Component.java
@@ -63,30 +63,7 @@ abstract public class Component extends ImageView{
 			
 		});
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public void setImageDrawable(Drawable d){
-		drawable = d;
-		board.notifyDrawableChanged();
-	}
-=======
 	
->>>>>>> parent of 424fb50... updated Component
-=======
-	
->>>>>>> parent of 424fb50... updated Component
-=======
-	
->>>>>>> parent of 424fb50... updated Component
-=======
-	
->>>>>>> parent of 424fb50... updated Component
-=======
-	
->>>>>>> parent of 424fb50... updated Component
 	public float getRequireElectricity(){
 		return this.requireElec;
 	}
@@ -179,35 +156,8 @@ abstract public class Component extends ImageView{
 		setRotation(rotation);
 	}
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public void setVisibility(boolean isVisible){
-		if(!isVisible){
-			board.removeComponent(this);
-		}
-=======
 	public final int getRotationId(){
 		return (int)Math.ceil((getRotation() / 90));
->>>>>>> parent of 424fb50... updated Component
-=======
-	public final int getRotationId(){
-		return (int)Math.ceil((getRotation() / 90));
->>>>>>> parent of 424fb50... updated Component
-=======
-	public final int getRotationId(){
-		return (int)Math.ceil((getRotation() / 90));
->>>>>>> parent of 424fb50... updated Component
-=======
-	public final int getRotationId(){
-		return (int)Math.ceil((getRotation() / 90));
->>>>>>> parent of 424fb50... updated Component
-=======
-	public final int getRotationId(){
-		return (int)Math.ceil((getRotation() / 90));
->>>>>>> parent of 424fb50... updated Component
 	}
 	
 	public final void moveTo(float x, float y){
